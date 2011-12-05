28d385aa2531eaa27d12795340a6324dd495a2ed
Kurtis Nusbaum <klnusbaum@gmail.com>
2011-12-04 19:09:04 -0600

    Wrote a test to make sure we're doing event ids corretly and not creating duplicates.

--- udjserver/udj/tests/event.py
+++ udjserver/udj/tests/event.py
@@ -213,10 +213,24 @@ class TestSetCurentSong(User1TestCase):
     oldCurrent = PlayedPlaylistEntry.objects.get(
       client_request_id=1, adder=3, event=1)
 
-class TestDoubleEventCreate(User1TestCase):
-  def testDoubleEventCreate(self):
+class TestDuplicateHostEventCreate(User1TestCase):
+  def testDuplicatHostEventCreate(self):
     partyName = "A Bitchn' Party"
     event = {'name' : partyName } 
     response = self.doJSONPut('/udj/events/event', json.dumps(event))
     self.assertEqual(response.status_code, 409)
+
+class TestDoubleEventCreate(User2TestCase):
+  def testDoubleEventCreate(self):
+    partyName = "A Bitchn' Party"
+    event = {'name' : partyName } 
+    response = self.doJSONPut('/udj/events/event', json.dumps(event))
+    self.assertEqual(response.status_code, 201)
+    eventId = json.loads(response.content)['event_id']
+    response = self.doDelete('/udj/events/'+str(eventId))
+    self.assertEqual(response.status_code, 200)
+    response = self.doJSONPut('/udj/events/event', json.dumps(event))
+    self.assertEqual(response.status_code, 201)
+    eventId = json.loads(response.content)['event_id']
+    response = self.doDelete('/udj/events/'+str(eventId))
 
