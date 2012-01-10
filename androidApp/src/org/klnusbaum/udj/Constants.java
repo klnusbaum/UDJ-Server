2de7d793162a854b17b1ce8702f60f54c03e78d8
Kurtis Nusbaum <klnusbaum@gmail.com>
2012-01-10 15:00:40 -0600

    Reorganized the constants file and made a small change to the event has ended message.

--- androidApp/src/org/klnusbaum/udj/Constants.java
+++ androidApp/src/org/klnusbaum/udj/Constants.java
@@ -21,16 +21,24 @@ package org.klnusbaum.udj;
 import android.net.Uri;
 
 public class Constants{
+  /** Constants used for storing account info */
   public static final String ACCOUNT_TYPE = "org.klnusbaum.udj";
   public static final String AUTHORITY = "org.klnusbaum.udj";
   public static final String USER_ID_DATA = "org.klnusbaum.udj.userid";
   public static final String LAST_EVENT_ID_DATA = "org.klnusbaum.udj.EventId";
   public static final String IN_EVENT_DATA = "org.klnusbaum.udj.InEvent";
-  public static final String ACCOUNT_EXTRA = "org.klnusbaum.udj.account";
-  public static final String EVENT_ID_EXTRA = "org.klnusbaum.udj.EventId";
+  public static final String EVENT_JOIN_STATUS = "eventJoinStatus";
+  public static final int EVENT_JOIN_FAILED = -1;
+  public static final int EVENT_JOIN_OK = 0;
   public static final long NO_EVENT_ID = -1;
   public static final int IN_EVENT_FLAG = 1;
   public static final int NOT_IN_EVENT_FLAG = 0;
+
+  /** Constants use for passing account related info in intents */
+  public static final String ACCOUNT_EXTRA = "org.klnusbaum.udj.account";
+  public static final String EVENT_ID_EXTRA = "org.klnusbaum.udj.EventId";
+
+  /** Constants for actions used throughout */
   public static final String ADD_REQUESTS_SYNCED = 
     "org.klnusbaum.udj.addRequestsSynced";
   public static final String LEFT_EVENT_ACTION = "org.klnusbaum.udj.LeftEvent";
@@ -40,10 +48,11 @@ public class Constants{
     "org.klnusbaum.udj.JoinedEvent";
   public static final String EVENT_JOIN_FAILED_ACTION = 
     "org.klnusbaum.udj.EventJoinFailed";
-  public static final String EVENT_JOIN_STATUS = "eventJoinStatus";
-  public static final int EVENT_JOIN_FAILED = -1;
-  public static final int EVENT_JOIN_OK = 0;
 
+  /** URI constants */
   public static final Uri EVENT_URI = new Uri.Builder().
     authority(Constants.AUTHORITY).appendPath("event").build();
+
+  /** Error constants */
+
 }
