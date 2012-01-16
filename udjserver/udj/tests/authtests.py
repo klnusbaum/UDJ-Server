e846854bb539fa63277c1de155d55f39d75ccdd4
Kurtis Nusbaum <klnusbaum@gmail.com>
2012-01-15 23:22:21 -0600

    Added another test to ensure that new tickets are issued.

--- udjserver/udj/tests/authtests.py
+++ udjserver/udj/tests/authtests.py
@@ -19,4 +19,20 @@ class AuthTest(TestCase):
     ticket = Ticket.objects.filter(user=testUser)
     self.assertEqual(response.__getitem__(getTicketHeader()), ticket[0].ticket_hash)
 
-
+  def testDoubleTicket(self):
+    client = Client()
+    response = client.post(
+      '/udj/auth', {'username': 'test2', 'password' : 'twotest'})
+    self.assertEqual(response.status_code, 200)
+    self.assertTrue(response.has_header(getTicketHeader()))
+    self.assertTrue(response.has_header(getUserIdHeader()))
+    testUser = User.objects.filter(username='test2')
+    self.assertEqual(
+      int(response.__getitem__(getUserIdHeader())), testUser[0].id)
+    ticket = Ticket.objects.filter(user=testUser)
+    self.assertEqual(response.__getitem__(getTicketHeader()), ticket[0].ticket_hash)
+    firstTicket = response[getTicketHeader()]
+    response = client.post(
+      '/udj/auth', {'username': 'test2', 'password' : 'twotest'})
+    secondTicket = response[getTicketHeader()]
+    self.assertNotEqual(firstTicket, secondTicket)
