1b19cfe20aae185d9dff8a46048e04e5d4062b46
Kurtis Nusbaum <klnusbaum@gmail.com>
2012-02-06 20:22:01 -0600

    modified the creatDBFile script to get rid of all user interaction now. Bictchn'

--- udjserver/createDBFile.py
+++ udjserver/createDBFile.py
@@ -2,12 +2,14 @@ import os
 import re
 
 def createDBFile():
-  filename = raw_input('Entry file name for database (must be absolute) :')
-  file = open('udjdb.py', 'w')
+  PATH = os.path.dirname(os.path.realpath(__file__))
+  filename = os.path.join('udjdb.py')
+  dbName = os.path.join('udjDebug.db')
+  file = open(filename, 'w')
   file.write("def getUDJDbSettings():\n")
   file.write("  return {'default': {\n")
   file.write("    'ENGINE': 'django.db.backends.sqlite3',\n ")
-  file.write("    'NAME': '" + filename + "',\n")
+  file.write("    'NAME': '" + dbName + "',\n")
   file.write("    'USER': '',\n")
   file.write("    'PASSWORD': '',\n")
   file.write("    'HOST': '',\n")
