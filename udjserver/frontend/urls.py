187370532b4d00df2a70b63adb042c52311e17e4
Kurtis Nusbaum <klnusbaum@gmail.com>
2012-02-24 21:52:16 -0600

    Frontend website is all working.

--- udjserver/frontend/urls.py
+++ udjserver/frontend/urls.py
@@ -1,8 +1,12 @@
 from django.conf.urls.defaults import patterns, include, url
+from django.contrib.auth.views import login, logout
 
 urlpatterns = patterns('frontend.views',
     (r'^$', 'home'),
-    (r'^signup/$', 'signup'),
+    #(r'^accounts/login/$', 'login'),
+    #(r'^accounts/logout/$', 'logout'),
+    (r'^registration/register/$', 'register'),
+    (r'^registration/thanks/$', 'thanks'),
 )
 
 
--- udjserver/frontend/views.py
+++ udjserver/frontend/views.py
@@ -1,19 +1,24 @@
 from django.template import Context, RequestContext, loader
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render_to_response
+from django.contrib.auth.forms import UserCreationForm
 # Create your views here.
 
 def home(request):
-  """
-  if request.method == 'POST':
-    form = ApplicationForm(request.POST)
-    if form.is_valid():
-      #process form here!
-      return HttpResponseRedirect('/thanks/')
-  else:
-    form = ApplicationForm()
-  """
   return render_to_response('home.html', {'page' : 'home'}, context_instance=RequestContext(request))
 
-def signup(request):
-  return render_to_response('signup.html', {'page' : 'signup'}, context_instance=RequestContext(request))
+def register(request):
+  if request.method == 'POST':
+    form = UserCreationForm(request.POST)
+    if form.is_valid():
+      new_user = form.save()
+      return HttpResponseRedirect("/registration/thanks/")
+  else:
+    form = UserCreationForm()
+  return render_to_response("registration/register.html", { 'page' : 'login', 'form': form, },
+      context_instance=RequestContext(request))
+
+def thanks(request):
+  return render_to_response("registration/thanks.html", {},
+      context_instance=RequestContext(request))
+
