eb1d538ff201544c693fb7aa28df3d3a57aa37f3
Kurtis Nusbaum <klnusbaum@bazaarsolitions.com>
2012-04-26 00:19:36 +0000

    Enabled some ticket admin stuff

--- udjserver/udj/admin.py
+++ udjserver/udj/admin.py
@@ -37,7 +37,7 @@ class VoteAdmin(admin.ModelAdmin):
   list_filter = ('playlist_entry__song__player', 'playlist_entry__state', 'user', 'weight')
 
 
-admin.site.register(Ticket)
+admin.site.register(Ticket, TicketAdmin)
 admin.site.register(Player)
 admin.site.register(PlayerPassword)
 admin.site.register(PlayerLocation)
