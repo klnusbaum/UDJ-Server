01fff8b9d518f1aed099a12bdf550f514c5c34bf
Kurtis Nusbaum <klnusbaum@gmail.com>
2012-05-06 14:41:23 -0500

    Parameterized the udjTestCaller code

--- udjTestCaller.py
+++ udjTestCaller.py
@@ -1,10 +1,13 @@
 from httplib import HTTPSConnection
 from urllib import urlencode
+import json
 
-requestUrl = "/udj/auth"
-conn = HTTPSConnection('www.udjplayer.com:4898')
-params = urlencode({'username' : 'test5', 'password' : 'fivetest'})
-authRequest = conn.request('POST', requestUrl, params)
-response = conn.getresponse()
-data = response.read()
-print data
+def getAuthToken(username, password):
+  requestUrl = "/udj/auth"
+  conn = HTTPSConnection('www.udjplayer.com:4898')
+  params = urlencode({'username' : username, 'password' : password})
+  authRequest = conn.request('POST', requestUrl, params)
+  response = conn.getresponse()
+  data = response.read()
+  response = json.loads(data)
+  return (response["ticket_hash"], response["user_id"])
