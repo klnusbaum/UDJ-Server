cd3ce242dd443bf9cb1cfa3a654bb0a3fb33a953
Kurtis Nusbaum <klnusbaum@bazaarsolutions.com>
2012-05-16 15:01:19 -0500

    Added some error code to help clients debugging

--- udjserver/udj/views/library.py
+++ udjserver/udj/views/library.py
@@ -54,8 +54,8 @@ def addSongs2Library(request, user_id, player_id, player):
 
   try:
     addSongs(libJSON, player)
-  except KeyError:
-    return HttpResponseBadRequest('Bad JSON\n' + request.raw_post_data)
+  except KeyError as e:
+    return HttpResponseBadRequest('Bad JSON\n An entry was missing the value: " + e)
   except ValueError:
     return HttpResponseBadRequest('Bad JSON\n' + request.raw_post_data)
   except AlreadyExistsError:
