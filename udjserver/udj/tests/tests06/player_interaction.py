82ed5aa5ee7795d1b44d63cde2f5a62998d74f9a
Kurtis Nusbaum <klnusbaum@gmail.com>
2012-08-05 15:39:48 -0500

    Added test for clearing kick flags

--- udjserver/udj/tests/tests06/player_interaction.py
+++ udjserver/udj/tests/tests06/player_interaction.py
@@ -3,6 +3,7 @@ from udj.models import Participant
 from datetime import datetime
 from udj.testhelpers.tests06.testclasses import ZachTestCase
 from udj.headers import DJANGO_PLAYER_PASSWORD_HEADER, FORBIDDEN_REASON_HEADER
+from udj.testhelpers.tests06.decorators import EnsureParticipationUpdated
 
 class BeginParticipateTests(ZachTestCase):
   def testSimplePlayer(self):
@@ -31,3 +32,13 @@ class BeginParticipateTests(ZachTestCase):
     response = self.doPut('/udj/0_6/players/7/users/user')
     self.assertEqual(response.status_code, 403)
     self.assertEqual(response[FORBIDDEN_REASON_HEADER], 'player-full')
+
+  @EnsureParticipationUpdated(8, 6)
+  def testClearKickFlag(self):
+    zach = Participant.objects.get(user__id=8, player__id=6)
+    self.assertEqual(zach.kick_flag, True)
+    response = self.doPut('/udj/0_6/players/6/users/user')
+    self.assertEqual(response.status_code, 201)
+    zach = Participant.objects.get(player__id=6, user__id=8)
+    self.assertEqual(zach.kick_flag, False)
+
